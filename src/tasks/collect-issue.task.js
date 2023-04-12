ebc9cf998fd7f8242c2fcd0e1628e8353000f536
Ta Duy Hoang <duyhoangptit@gmail.com>
2023-04-12 19:58:32 +0700

    update on, off task

--- src/tasks/collect-issue.task.js
+++ src/tasks/collect-issue.task.js
@@ -4,7 +4,9 @@ class CollectIssueTask {
 
     execute() {
         return cron.schedule("*/10 * * * * *", function () {
-            if (!process.env.TASK_ENABLE) return;
+            if (process.env.TASK_ENABLE === 'false') {
+                return;
+            }
 
             console.log("---------------------");
             console.log("running a task every 10 seconds");
